f409bb6d3f38c19d47d09dd65942cffc4e9880af
spartan <spartan@SPARTAN.lan>
2022-03-16 12:56:08 -0500

    change task

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -5,9 +5,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-zr@0xs0hjmo6i%t!g=b_+i@cud%rhkbi&zby+2rm9$&wtfxpyw'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
