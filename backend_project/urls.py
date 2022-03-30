4b0a55838874a3a15fa58a71a1a04bbe4aad51b9
spartan <spartan@SPARTAN.lan>
2022-03-30 16:17:56 -0500

    add swagger

--- backend_project/urls.py
+++ backend_project/urls.py
@@ -5,21 +5,21 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from rest_framework import permissions
-# from drf_yasg.views import get_schema_view
-# from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="CriptoLine",
-#         default_version='v1',
-#         description="URL API Backend",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="contact@snippets.local"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     public=True,
-#     permission_classes=[permissions.AllowAny],
-# )
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CriptoLine",
+        default_version='v1',
+        description="URL API Backend",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,9 +28,9 @@ urlpatterns = [
     path('', include('task.urls')),
     path('', include('center.urls')),
     
-    # re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    # re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    # re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] 
 
 if settings.DEBUG:
